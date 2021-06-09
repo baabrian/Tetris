32747987174ed658656aa8c0945d5683ea10cb30
Brian Nguyen <brianlnguyen12@gmail.com>
2021-06-08 17:24:08 -0700

    Update tetrisReducer.ts

--- tetris/src/store/reducers/tetrisReducer.ts
+++ tetris/src/store/reducers/tetrisReducer.ts
@@ -1,11 +1,12 @@
 import { Tetris } from '../../game/Tetris';
 import { tetrisResetGame } from './reducerUtilities/tetrisReducerUtil';
+import { ActionType, RESET_GAME } from '../actions/types';
 
-
-export const tetrisReducer = (state = Tetris, action) => {
+export const tetrisReducer = (state = Tetris, action: ActionType) => {
   switch (action.type) {
+    case RESET_GAME:
     default: {
-      return tetrisResetGame(state);
+      return state;
     }
   }
 };
