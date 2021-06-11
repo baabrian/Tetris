4820a63bbd6d8cb93b0cd7cdc678673a6ecb3496
Brian Nguyen <brianlnguyen12@gmail.com>
2021-06-10 23:36:01 -0700

    updates

--- tetris/src/components/tetris/index.tsx
+++ tetris/src/components/tetris/index.tsx
@@ -7,11 +7,13 @@ import { RootStore } from '../../store';
 import { Grid } from '@material-ui/core';
 
 export const Tetris: React.FC = (): JSX.Element => {
-  const { board, nextShape, shapesQueue } = useSelector<RootStore, IGame>(state => state.tetris)
+  const tetris = useSelector<RootStore, IGame>(state => state.tetris)
+  console.log(tetris);
+  const { board, nextShape, shapesQueue } = tetris
 
   return (
-    <Grid container direction='column' style={{ height: '100%', width: '75%', padding:'3rem', borderStyle:'double' }}>
-      <Display board={board} nextShape={nextShape} shapesQueue={shapesQueue} />
+    <Grid container direction='column' style={{ height: '100%', width: '75%', padding: '3re', borderStyle: 'double' }}>
+      <Display board={board} nextShape={nextShape} shapesQueue={shapesQueue} currentShape={tetris.currentShape} />
       <Keyboard />
     </Grid>
   );
