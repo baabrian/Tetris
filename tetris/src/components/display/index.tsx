f2d17cc749b62c72fc709297f57fbdf610a387d8
Brian Nguyen <brianlnguyen12@gmail.com>
2021-06-10 23:34:47 -0700

    update for ICurrentShape sup

--- tetris/src/components/display/index.tsx
+++ tetris/src/components/display/index.tsx
@@ -1,6 +1,7 @@
 import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { EMPTYSLOTT, SHAPESKIND } from '../../ts/enums';
+import { ICurrentShape } from '../../ts/interfaces/ICurrentShape';
 import { IShape } from '../../ts/interfaces/IShape';
 import { Board } from '../board'
 
@@ -8,16 +9,16 @@ interface OwnProps {
     board: (EMPTYSLOTT | SHAPESKIND)[][],
     nextShape: IShape,
     shapesQueue: IShape[],
+    currentShape: ICurrentShape
 }
 
 type Props = OwnProps
 
 export const Display: React.FC<Props> = (props): JSX.Element => {
-    const { board, nextShape, shapesQueue } = props
-
+    const { board, nextShape, shapesQueue, currentShape } = props
     return (
         <Grid container >
-            <Board board={board} />
+            <Board board={board} currentShape={currentShape} />
             <Grid item direction='column' style={{ flex: '1' }} >
                 <Typography align='center'>Next Shape container</Typography>
                 <Typography align='center'>Shapes Queue Container</Typography>
